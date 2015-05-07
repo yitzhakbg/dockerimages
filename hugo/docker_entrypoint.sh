9e348c5c1dda65ec615334c91338617a338f3594
Yitzhak Bar Geva <yitzhakbargeva@gmail.com>
2015-05-07 19:06:23 +0300

    smarter docker build and ehanced docker_entrypoint.sh

--- hugo/docker_entrypoint.sh
+++ hugo/docker_entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 #remove possibly existing site
-rm -rf /site/public/*
+# rm -rf /site/public/*
 
-#run hugo with the suplplied arguments
-echo running: hugo "$@"
-exec hugo "$@"
+# Check for no arguments -> run default
+if [[ $# -eq 0 ]]; then
+  echo hugo -w
+  exec hugo -w
+fi
+
+# Regexs for checking commands
+valid_switch='(^\-[bdDhstvw])'
+valid_command='(server|version|config|check|benchmark|new|help)'
+
+if [[ "$1" =~ $valid_command || "$1" =~ $valid_switch ]]; then
+  echo hugo "$@"
+  exec hugo "$@" # 1st parameter is a valid hugo command
+fi
+
+# Just run anything else
+echo "$@"
+exec "$@"
